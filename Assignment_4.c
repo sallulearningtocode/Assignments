f2afd250639cdd40ec7d79dbafe4a14143f78b90
Salman Khan <111654354+sallulearningtocode@users.noreply.github.com>
2022-10-07 16:03:01 +0530

    Add files via upload

---
+++ Assignment_4.c
@@ -0,0 +1,94 @@
+#include<stdio.h>
+ /*1.Program to Print MYSIRG 5 Times on the Screen
+int main()
+{
+    int i=1;
+    while(i<=5)
+    {
+        printf("MySirG\n");
+        i++;
+    }
+}*/
+
+/*2.Program to print first 10 natural numbers
+int main()
+{
+    for(int i=1; i<=10;i++)
+    {
+        printf("%d\n",i);
+    }
+}*/
+
+/*3.Program to print frist 10 natural numbers in reverse order
+int main()
+{
+    for(int i=10; i>=1;i--)
+    {
+        printf("%d\n",i);
+    }
+}*/
+
+/*4.Program to print 10 Natural Odd Numbers
+int main()
+{
+    for(int i=1; i<=10; i++)
+    {
+        printf("%d\n",i*2-1);
+    }
+}*/
+
+/*5.Program to Print 10 Odd Natural Numbers in Reverse Order
+int main()
+{
+    for(int i=10; i>=1; i--)
+    {
+        printf("%d\n",i*2-1);
+    }
+}*/
+
+/*6.Program to Print 10 even natural Numbers
+int main()
+{
+    for(int i=1; i<=10; i++)
+    {
+        printf("%d\n",i*2);
+    }
+}*/
+
+/*7.Program to print 10 even natural numbers in reverse order 
+int main()
+{
+    for(int i=10; i>=1; i--)
+    {
+        printf("%d\n",i*2);
+    }
+}*/
+
+/*8.Program to Print Squares of first 10 natural numbers 
+int main()
+{
+    for(int i=1; i<=10; i++)
+    {
+        printf("%d ",i*i);
+    }
+    
+}*/
+
+/*9.Proram to Print Cubes of First 10 Natural Numbers
+int main()
+{
+    for(int i=1; i<=10; i++)
+    {
+       printf("%d ",i*i*i);
+    }
+    
+}*/
+
+/*10.Program to Print table of 5.
+int main()
+{
+    for(int i=1; i<=10; i++)
+    {
+        printf("5 * %d = %d\n",i,5*i);
+    }
+}
