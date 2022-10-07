99d88264a0dadbb82b8520d8da66bd679df8ec95
Salman Khan <111654354+sallulearningtocode@users.noreply.github.com>
2022-10-07 11:29:19 +0530

    Add files via upload

---
+++ Assignment_1.c
@@ -0,0 +1,104 @@
+/*1-Program to Print HEllo Students on the Screen
+#include<stdio.h>
+int main()
+{
+    printf("Hello Students");
+}*/
+
+/*2-Program to Print Hello in the first line Students in the Second Line
+#include<stdio.h>
+int main()
+{
+    printf("Hello\nStudents");
+}*/
+
+/*3-Program to Print "MYSIRG" on the Screen
+#include<stdio.h>
+int main()
+{
+    printf("\"My Sirg\"");
+}*/
+
+/*4-Program to Find Area of The Circle.
+#include<stdio.h>
+int main()
+{
+    float a,r;
+    printf("Enter Radius\n");
+    scanf("%f",&r);
+    a=3.14*r*r;
+    printf("Area of Circle is %f",a);
+}*/
+
+/*5-Program to print length of the String.
+#include<stdio.h>
+int main()
+{
+    printf("%d",printf("Hello Students\n"));
+}*/
+
+/*6-Program to print User name in double quotes
+#include<stdio.h>
+int main()
+{
+    printf("\"Salman Khan\"");
+}*/
+
+/*7-Program to print "%d" on the Screen.
+#include<stdio.h>
+int main()
+{
+    printf("\"%\"");
+    printf("\d\"");
+}*/
+
+/*8.Program to Print \n on the screen.
+#include<stdio.h>
+int main()
+{
+    printf("\\n");
+}*/
+
+/*9.Porgram to print \\ on the screen.
+#include<stdio.h>
+int main()
+{
+    printf("\\""\\");
+}
+
+/*10.
+-Program to take date as an input and display the result
+#include<stdio.h>
+int main()
+{
+    int dd,mm,yy;
+    printf("Enter Date\n");
+    scanf("%d/%d/%d",&dd,&mm,&yy);
+    printf("Day-%d, Month-%d, Year-%d",dd,mm,yy);
+
+}*/
+
+/*10.Program to take date as an input in below given format and displlay the result as given below
+int main()
+{
+    int d,m,y;
+    printf("Enter Date Month and Year\n");
+    scanf("%d%d%d",&d,&m,&y);
+    printf("Day -%d, Month-%d, Year-%d",d,m,y);
+}*/
+
+/*11-Program to take time as and convert the time format in words.
+#include<stdio.h>
+int main()
+{
+    int h,m;
+    printf("Enter Date\n");
+    scanf("%d:%d",&h,&m);
+    printf("\"%d Hour and %d Minutes\"",h,m);
+}*/
+
+/*12-Program to find output.
+    Output:
+    ineuron
+    7
+}*/
